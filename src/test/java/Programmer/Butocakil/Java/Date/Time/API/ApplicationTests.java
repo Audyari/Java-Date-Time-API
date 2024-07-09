dbcad613c59e458e4dd08f1d7b6797ef91999f12
Audyari Wiyono <audy123ari@gmail.com>
2024-07-09 09:59:22 +0700

    Date

---
+++ src/test/java/Programmer/Butocakil/Java/Date/Time/API/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Programmer.Butocakil.Java.Date.Time.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
